8cb971cae642ed5f705cb7fe54414e4f5ba9923f
khanhjoi <khanhb2003835@student.ctu.edu.vn>
2022-11-14 11:53:37 +0700

    create filter sort and pagination

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,11 +1,69 @@
  const Products = require('../models/productModel');
 
- const productCtrl = {
+// Filtering, sorting and paginating
+
+class APIfeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    filtering() {
+        const queryObj = {...this.queryString}; // queryStringq = req.query
+        
+        // console.log({before: queryObj}); //before delete page
+
+        const excludedFields = ['page' , 'sort', 'limit']; // clear Fields to query
+        excludedFields.forEach(el => delete(queryObj[el]));
+
+        // console.log({after: queryObj}); //after deletePage
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match); // define query in mongoose
+
+        // gte = greater than or equal
+        // lte = lesser than or equal
+        // lt = lesser than 
+        // gt = greater than 
+        // regex = find characters in string
+
+        this.query.find(JSON.parse(queryStr)); // find query in mongooseDB
+
+        return this;
+    }
+
+    sorting(){
+        if(this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            console.log(sortBy)
+            this.query = this.query.sort(sortBy);
+        }else {
+            this.query = this.query.sort('-createdAt')
+        }
+        return this;
+    }
+
+    paginating(){
+        // define page and skip page
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit *1 || 6;
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit);
+        return this;
+    }
+}
+
+const productCtrl = {
     getProducts: async (req, res,) => {
         try {
+            const features = new APIfeatures(Products.find(), req.query).filtering().sorting().paginating()
             // get all products
-            const products = await Products.find()
-            res.json(products);
+            const products = await features.query;
+            res.json({
+                status: 'success',
+                result: products.length,
+                products: products
+            });
         } catch (err) {
             return res.status(500).json({msg: err.message});
         }
